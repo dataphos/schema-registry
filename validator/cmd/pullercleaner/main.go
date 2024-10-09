8ba3742d3d60079e013a030c0a9d705f34f183d7
Skenktio <josip.skender@syntio.net>
2024-10-09 14:06:08 +0200

    refactor:  removed deprecated usage of rand.seed in centralconsumer/main.go and pullecleaner/main.go

--- validator/cmd/pullercleaner/main.go
+++ validator/cmd/pullercleaner/main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"flag"
-	"math/rand"
-	"time"
-
 	"github.com/dataphos/aquarium-janitor-standalone-internal/internal/janitorctl"
 )
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
 
 func main() {
 	configFile := flag.String("f", "", "toml file containing configuration of working environment")
